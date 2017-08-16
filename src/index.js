bf1ee5bfcb983dae203cedee9281f133636b39fc
papistan <jpapisan@gmail.com>
2017-08-16 16:59:34 -0700

    install youtube search npm, establish index files and shell component files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDZJ2V4RMEyJXggpVxjwq5rYaSSxIUfoCw';
+// create a new component. This component should produce some HTML
+// JSX 
+const App = () => {
+	return <div>Hi!!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Take this component's generated HTML and put it on the page (in the DOM) 
+ReactDOM.render(<App />, document.querySelector('.container'));
